ab0493b1473e35ae3194693fe54c4dcb0e42b8b2
frankkim92 <syncstar9@gmail.com>
2024-11-07 09:38:36 +0900

    uninstall browser-router, package.json homepage

--- src/main.tsx
+++ src/main.tsx
@@ -6,13 +6,10 @@ import { createRoot } from 'react-dom/client';
 import { Routes } from '@generouted/react-router';
 import 'nprogress/nprogress.css';
 import '@/style/nprogress.css';
-import { BrowserRouter } from 'react-router-dom';
 
 createRoot(document.getElementById('root')!).render(
-  <BrowserRouter basename="/resume">
-    <HelmetProvider>
-      <Routes />
-    </HelmetProvider>,
-  </BrowserRouter>,
+  <HelmetProvider>
+    <Routes />
+  </HelmetProvider>,
 
 );
